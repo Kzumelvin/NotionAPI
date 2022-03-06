d31108a62531284cdbdce13c0907a57f999e771c
Kzumelvin <kevin.haberl@icloud.com>
2022-03-06 19:16:29 +0100

    adding appending blocks

---
+++ blocks/blocks.py
@@ -0,0 +1,44 @@
+"""Notion blocks.
+
+This module helps to modify Notion blocks.
+
+Authors:
+    Kevin Haberl <mail@beerpongturnier.de>
+
+"""
+
+import requests
+
+def appendBlockChildren(block_id, headers, *payloads):
+    """Appends a item to a block or a page.
+
+        The block ID of a page is the page ID
+    	payloads have the following structure:
+    	{
+          "object": "block", << always necessary
+          "type": "paragraph", << the object typ to be appended
+          "paragraph": { << the defintion of the object
+            "text": [{
+                "type": "text",
+                "text":
+                    {
+                        "content": "– Notion API Team",
+                        "link": {
+                            "type": "url",
+                            "url": "https://twitter.com/NotionAPI"
+                            }
+                    }
+                }]
+        }
+
+    more to https://developers.notion.com/reference
+    """
+    url = f"https://api.notion.com/v1/blocks/{block_id}/children"
+    payload_items = []
+    for item in payloads:
+        payload_items.append(item)
+    payload = {
+        "children": payload_items
+    }
+    response = requests.request("PATCH", url, json=payload, headers=headers)
+    return response
